961611ecfe2fe6ad4cf79c0af54ea66f04dd0d20
ShyEdge <shyshy200309@gmail.com>
2025-07-30 16:32:02 +0800

    modify SCH_AGENT_PARAMETERS

--- dependency/core/applications/pedestrian_detection/pedestrian_detection_without_tensorrt/pedestrian_detection_yolov8.py
+++ dependency/core/applications/pedestrian_detection/pedestrian_detection_without_tensorrt/pedestrian_detection_yolov8.py
@@ -14,12 +14,8 @@ class PedestrianDetectionYoloV8:
         self.target_class_ids = set(self.class_name_to_id.values())
 
     def infer(self, img: np.ndarray):
-        """
-        对图像进行推理，输出车辆相关的检测框、置信度、类别ID
-        :param img: 输入图像，格式为 OpenCV 的 BGR ndarray
-        :return: result_boxes, result_scores, result_classid
-        """
-        results = self.model(img)[0]  # 取第一个batch结果
+
+        results = self.model(img)[0]  
 
         boxes = results.boxes.xyxy.cpu().numpy()
         scores = results.boxes.conf.cpu().numpy()
@@ -27,7 +23,7 @@ class PedestrianDetectionYoloV8:
 
         keep_idx = [i for i, cid in enumerate(class_ids) if cid in self.target_class_ids]
 
-        result_boxes = np.round(boxes[keep_idx]).astype(int)   # ⭐ 转为整数
+        result_boxes = np.round(boxes[keep_idx]).astype(int)   
         result_scores = scores[keep_idx]
         result_classid = class_ids[keep_idx]
 
