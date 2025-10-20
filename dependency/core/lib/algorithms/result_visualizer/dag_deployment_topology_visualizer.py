a2d7a273543d1f6eaae6944709581cd972bc0ca2
onecheck <1548767361@qq.com>
2025-10-21 00:47:06 +0800

    test: test for abnormal visualization time

--- dependency/core/lib/algorithms/result_visualizer/dag_deployment_topology_visualizer.py
+++ dependency/core/lib/algorithms/result_visualizer/dag_deployment_topology_visualizer.py
@@ -20,9 +20,13 @@ class DAGDeploymentTopologyVisualizer(TopologyVisualizer, abc.ABC):
         for node_info in result.values():
             LOGGER.debug(f'[DEBUG] start node_info: {node_info["service"]["service_name"]}')
             service = node_info["service"]
+            LOGGER.debug(f'[DEBUG] got service')
             service.pop("execute_device")
+            LOGGER.debug(f'[DEBUG] pop execute device')
             service_name = service["service_name"]
+            LOGGER.debug(f'[DEBUG] got service name')
             service["data"] = '\n'.join(KubeConfig.get_nodes_for_service(service_name))
+            LOGGER.debug(f'[DEBUG] got service data')
             LOGGER.debug(f'[DEBUG] end node_info: {node_info["service"]["service_name"]}')
         LOGGER.debug('[DEBUG] end dag deployment topology visualizer')
 
