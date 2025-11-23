e930c138fbbcc65014608dedbad88207969f76fd
onecheck <1548767361@qq.com>
2025-11-23 19:27:30 +0800

    feat: update jp6 supplementary scripts

---
+++ hack/tools/pull_jp6.sh
@@ -0,0 +1,10 @@
+docker pull "$REG"/dayuhub/pedestrian-detection:v1.2-jp6
+docker tag "$REG"/dayuhub/pedestrian-detection:v1.2-jp6 "$REG"/dayuhub/pedestrian-detection:v1.2
+docker pull "$REG"/dayuhub/license-plate-recognition:v1.2-jp6
+docker tag "$REG"/dayuhub/license-plate-recognition:v1.2-jp6 "$REG"/dayuhub/license-plate-recognition:v1.2
+docker pull "$REG"/dayuhub/vehicle-detection:v1.2-jp6
+docker tag "$REG"/dayuhub/vehicle-detection:v1.2-jp6 "$REG"/dayuhub/vehicle-detection:v1.2
+docker pull "$REG"/dayuhub/exposure-identification:v1.2-jp6
+docker tag "$REG"/dayuhub/exposure-identification:v1.2-jp6 "$REG"/dayuhub/exposure-identification:v1.2
+docker pull "$REG"/dayuhub/category-identification:v1.2-jp6
+docker tag "$REG"/dayuhub/category-identification:v1.2-jp6 "$REG"/dayuhub/category-identification:v1.2
