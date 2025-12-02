15f0102503eba0585515b3df70b902cd1e62cacf
onecheck <1548767361@qq.com>
2025-12-02 11:49:36 +0800

    test: test for health checking

--- dependency/core/lib/common/health.py
+++ dependency/core/lib/common/health.py
@@ -1,5 +1,5 @@
 from core.lib.network import NodeInfo, PortInfo, NetworkAPIPath, NetworkAPIMethod, http_request, merge_address
-from core.lib.common import SystemConstant
+from core.lib.common import SystemConstant, LOGGER
 
 
 class HealthChecker:
@@ -7,6 +7,7 @@ class HealthChecker:
     def check_processors_health():
         """check if all processors is healthy"""
         nodes = NodeInfo.get_edge_nodes() + [NodeInfo.get_cloud_node()]
+        LOGGER.debug(f'[HEALTH CHECK] health checking nodes: {nodes}')
         for node in nodes:
             node_processor_health_address = merge_address(
                 NodeInfo.hostname2ip(node),
@@ -15,5 +16,8 @@ class HealthChecker:
             response = http_request(url=node_processor_health_address,
                                     method=NetworkAPIMethod.CONTROLLER_CHECK)
             if not response or response.get('status', '') != 'ok':
+                LOGGER.debug(f'[HEALTH CHECK] node {node} processor health check failed.')
                 return False
+            LOGGER.debug(f'[HEALTH CHECK] node {node} processor health check succeed.')
+
         return True
