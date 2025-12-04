739f986c6d809f72c6cef151c6542a8e492aa097
onecheck <1548767361@qq.com>
2025-12-04 19:36:08 +0800

    feat: add hedger deployment and offloading plan

--- dependency/core/lib/algorithms/schedule_agent/hedger_agent.py
+++ dependency/core/lib/algorithms/schedule_agent/hedger_agent.py
@@ -1,6 +1,8 @@
 import abc
+import copy
 
-from core.lib.common import ClassFactory, ClassType, GlobalInstanceManager, ConfigLoader, Context
+from core.lib.common import ClassFactory, ClassType, GlobalInstanceManager, ConfigLoader, Context, LOGGER, \
+    KubeConfig, TaskConstant
 from core.lib.content import Task
 from core.lib.algorithms.shared.hedger import Hedger
 
@@ -16,6 +18,7 @@ class HedgerAgent(BaseAgent, abc.ABC):
 
         self.system = system
         self.agent_id = agent_id
+        self.cloud_device = system.cloud_device
 
         self.default_configuration = None
         self.default_offloading = None
@@ -38,14 +41,39 @@ class HedgerAgent(BaseAgent, abc.ABC):
             self.hedger.register_offloading_agent()
 
     def get_schedule_plan(self, info):
+        source_id = info['source']['id']
         source_edge_device = info['source_device']
         all_edge_devices = info['all_edge_devices']
+        cloud_device = self.cloud_device
+        all_devices = [*all_edge_devices, cloud_device]
         dag = info['dag']
 
         self.hedger.register_logical_topology(Task.extract_dag_from_dag_deployment(dag))
         self.hedger.register_physical_topology(all_edge_devices, source_edge_device)
 
-        return None
+        configuration = copy.deepcopy(self.default_configuration)
+        offloading = self.hedger.get_offloading_plan()
+        if not offloading:
+            offloading = copy.deepcopy(self.default_offloading)
+            LOGGER.warning('No offloading plan from Hedger, use default offloading policy.')
+
+        policy = {}
+        policy.update(configuration)
+        service_info = KubeConfig.get_service_nodes_dict()
+
+        for service_name in dag:
+            if service_name in service_info and service_name in offloading \
+                    and offloading[service_name] in all_devices:
+                dag[service_name]['service']['execute_device'] = offloading[service_name]
+            elif service_name == TaskConstant.START.value:
+                dag[service_name]['service']['execute_device'] = source_edge_device
+            else:
+                dag[service_name]['service']['execute_device'] = cloud_device
+
+        policy.update({'dag': dag})
+
+        LOGGER.info(f'[Offloading] (source {source_id}) Schedule policy: {policy}')
+        return policy
 
     def run(self):
         pass
