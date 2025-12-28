f21f9ed8f658c9a7441d96d4031dad95cc3ef037
onecheck <1548767361@qq.com>
2025-12-29 02:40:51 +0800

    feat: add memory perception in monitor

--- dependency/core/lib/algorithms/parameter_monitor/model_memory_monitor.py
+++ dependency/core/lib/algorithms/parameter_monitor/model_memory_monitor.py
@@ -1,6 +1,6 @@
 import abc
 
-from core.lib.common import ClassFactory, ClassType, KubeConfig, ServiceConfig
+from core.lib.common import ClassFactory, ClassType, KubeConfig, ServiceConfig, LOGGER
 from core.lib.network import NodeInfo
 from .base_monitor import BaseMonitor
 
@@ -18,7 +18,7 @@ class ModelMemoryMonitor(BaseMonitor, abc.ABC):
     def get_model_memory(self):
         KubeConfig.force_refresh()
         pods_list = KubeConfig.get_pods_on_node(self.local_device)
-        print(f'*****device: {self.local_device}, pods_list: {pods_list}')
+        LOGGER.debug(f'*****device: {self.local_device}, pods_list: {pods_list}')
         pod_memory_dict = KubeConfig.get_pod_memory_from_metrics(pods_list)
 
         service_memory_dict = {
