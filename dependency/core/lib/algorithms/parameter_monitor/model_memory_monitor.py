f74dd909ff3ece25268e60e66b73980c713ebe4b
onecheck <1548767361@qq.com>
2025-12-29 00:46:28 +0800

    feat: add memory perception in monitor

--- dependency/core/lib/algorithms/parameter_monitor/model_memory_monitor.py
+++ dependency/core/lib/algorithms/parameter_monitor/model_memory_monitor.py
@@ -20,9 +20,11 @@ class ModelMemoryMonitor(BaseMonitor, abc.ABC):
         pods_list = KubeConfig.get_pods_on_node(self.local_device)
         pod_memory_dict = KubeConfig.get_pod_memory_from_metrics(pods_list)
 
-        service_memory_dict = {ServiceConfig.map_pod_name_to_service(pod): memory
-                               for pod, memory in pod_memory_dict.items()}
-        service_memory_dict.pop(None)  # Remove pods that are not mapped to any service
+        service_memory_dict = {
+            svc: memory
+            for pod, memory in pod_memory_dict.items()
+            if (svc := ServiceConfig.map_pod_name_to_service(pod)) is not None
+        }
         return service_memory_dict
 
     def get_parameter_value(self):
